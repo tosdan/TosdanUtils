46443f1ae6674feb75dc657d5323078cceaaec4c
Daniele T <stdanielets@gmail.com>
2013-07-11 23:22:54 +0200

    New options: closeConn,autoCommit,commit,rollBack

--- WEB-INF/src/com/github/tosdan/utils/sql/BasicDAO.java
+++ WEB-INF/src/com/github/tosdan/utils/sql/BasicDAO.java
@@ -2,6 +2,7 @@ package com.github.tosdan.utils.sql;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.util.List;
 import java.util.Map;
 
@@ -19,13 +20,14 @@ import org.apache.commons.dbutils.handlers.MapListHandler;
 /**
  * 
  * @author Daniele
- *
+ * @version 0.2.0-b2013-07-11
  */
 public class BasicDAO
 {
 	private DataSource dataSource;
 	private ConnectionProvider provider;
 	private Connection conn;
+	private boolean closeConn;
 	
 	/**
 	 * 
@@ -34,6 +36,7 @@ public class BasicDAO
 	@Deprecated
 	public BasicDAO( DataSource dataSource ) {
 		this.dataSource = dataSource;
+		this.setCloseConn( true );
 	}
 
 	/**
@@ -42,6 +45,7 @@ public class BasicDAO
 	 */
 	public BasicDAO( ConnectionProvider provider ) {
 		this.provider = provider;
+		this.setCloseConn( true );
 	}
 	
 	/**
@@ -50,8 +54,100 @@ public class BasicDAO
 	 */
 	public BasicDAO( Connection conn ) {
 		this.conn = conn;
+		this.setCloseConn( true );
+	}
+	
+	/**
+	 * 
+	 * @param closeConn
+	 * @return Restituisce l'oggetto stesso su cui si esegue il metodo.
+	 */
+	public BasicDAO setCloseConn( boolean closeConn ) {
+		this.closeConn = closeConn;
+		return this;
 	}
 
+	/**
+	 * 
+	 * @param b
+	 * @return
+	 * @throws BasicDAOException
+	 */
+	public BasicDAO setAutoCommit(boolean b) throws BasicDAOException {
+		try {
+			if (this.conn != null)
+				this.conn.setAutoCommit( b );
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il setAutoCommit().", e );
+		}
+		
+		return this;
+	}
+	
+	/**
+	 * 
+	 * @throws BasicDAOException
+	 */
+	public void commit() throws BasicDAOException {
+		try {
+			this.conn.commit();
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il commit().", e );
+		}
+	}
+	
+	/**
+	 * 
+	 * @throws BasicDAOException
+	 */
+	public void rollBack() throws BasicDAOException {
+		try {
+			this.conn.rollback();
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il rollBack().", e );
+		}
+	}
+	
+	/**
+	 * 
+	 * @param savepoint 
+	 * @throws BasicDAOException
+	 */
+	public void rollBack(Savepoint savepoint) throws BasicDAOException {
+		try {
+			this.conn.rollback( savepoint );
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il rollBack().", e );
+		}
+	}
+	
+	/**
+	 * 
+	 * @return 
+	 * @throws BasicDAOException
+	 */
+	public Savepoint setSavepoint() throws BasicDAOException {
+		try {
+			return this.conn.setSavepoint();
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il setSavepoint().", e );
+		}
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @return 
+	 * @throws BasicDAOException
+	 */
+	public Savepoint setSavepoint(String name) throws BasicDAOException {
+		try {
+			return this.conn.setSavepoint( name );
+		} catch ( SQLException e ) {
+			throw new BasicDAOException( "Errore durante il setSavepoint(String name).", e );
+		}
+	}
+	
 	/**
 	 * 
 	 * @param sql
@@ -158,7 +254,8 @@ public class BasicDAO
 		} catch ( SQLException e ) {
 			throw new BasicDAOException( "Errore di accesso al database.", e );
 		} finally {
-			DbUtils.closeQuietly( conn );
+			if (closeConn)
+				DbUtils.closeQuietly( conn );
 		}
 		
 	}
@@ -195,4 +292,22 @@ public class BasicDAO
 
 		return conn;
 	}
+	
+	/**
+	 * Chiude la connessione (se non nulla) nascondendo evenutali eccezioni.
+	 */
+	public void closeQuietly() {
+		DbUtils.closeQuietly( this.conn );
+		
+	}
+	
+	/**
+	 * Chiude la connessione senza alcun controllo preventivo. Eventuali controlli di valore null sono lasciato al chiamante.
+	 * @throws SQLException
+	 */
+	public void close() throws SQLException {
+		this.conn.close();
+		
+	}
+	
 }
