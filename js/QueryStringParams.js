99ce046f17da00a84dfa3aefeaeb90e48eb15fe0
Daniele T <stdanielets@gmail.com>
2013-08-09 18:48:18 +0200

    fixed FOR into paramsToMapsArray

--- js/QueryStringParams.js
+++ js/QueryStringParams.js
@@ -1,6 +1,6 @@
 /**
  * @author Daniele
- * @version 0.1.0b2013-08-09
+ * @version 0.1.1b2013-08-09
  */
 window.Q$tring = {
     /**
@@ -18,7 +18,8 @@ window.Q$tring = {
 	    	var matchedParam = decode(match[1]) ,
 	    		matchedValue = decode(match[2]) ,
 	    		valueStored = params[matchedParam];
-	    	
+
+//			console.log(matchedParam + ' - ' + matchedValue);
 	    	if ( valueStored ) {
 	    		if (typeof valueStored === 'string') {
 	    			var tempArray = new Array();
@@ -42,17 +43,18 @@ window.Q$tring = {
 	 */
 	paramsToMapsArray: function(paramsObject) {
 		var parametri = new Array(); 
-	
+//		console.log(paramsObject);
 		for(key in paramsObject) {
 			var valore = paramsObject[key];
 			
-	//		console.log(key + ' - ' + valore);
+//			console.log(key + ' - ' + valore);
 			if (typeof valore === 'string') {
 				parametri.push( { name: key, value: valore } );
 			} else {
 				// Se l'oggetto e' un array, viene aggiunta una coppia key->valore per ogni elemento contento nell'array
-				for(key in valore) {
-					parametri.push( { name: key, value: valore[key] } );
+				for(var i = 0 ; i < valore.length ; i++) {
+//					console.log(key + ' - ' + valore[i]);
+					parametri.push( { name: key, value: valore[i] } );
 				}
 			}
 		}
