aba231e187a350aa8d1bee15c124dae03cd5a2a1
Daniele T <stdanielets@gmail.com>
2013-09-01 17:40:27 +0200

    added findMap, sortMapList, keyedMapToMapList

--- WEB-INF/src/com/github/tosdan/utils/varie/MapUtils.java
+++ WEB-INF/src/com/github/tosdan/utils/varie/MapUtils.java
@@ -1,5 +1,8 @@
 package com.github.tosdan.utils.varie;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -7,10 +10,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
+import com.github.tosdan.utils.stringhe.StrUtils;
+
 /**
  * 
  * @author Daniele
- * @version 0.0.3-b2013-08-29
+ * @version 0.0.4-b2013-08-30
  */
 public class MapUtils
 {
@@ -60,15 +65,116 @@ public class MapUtils
 
 	/**
 	 * Fonde due o piu' mappe.
-	 * @param maps
+	 * @param maps Elenco di mappe separate da virgole
 	 * @return
 	 */
-	public static Map<String, Object> mergeMaps(Map<String, ? extends Object>... maps) {
-		Map<String, Object> retVal = new HashMap<String, Object>();
+	public static <T extends Object> Map<String, T> mergeMaps(Map<String, T>... maps) {
+		Map<String, T> retVal = new HashMap<String, T>();
 		for( int i = 0 ; i < maps.length ; i++ ) {
 			if (maps[i] != null)
 				retVal.putAll( maps[i] );
 		}
 		return retVal;
+	}
+	
+	
+
+	/**
+	 * Trasforma una Mappa di Mappe in una Lista di Mappe ordinata.
+	 * @param keyedMap mappa da trasformare
+	 * @return
+	 */
+	public static <T extends Object> List<Map<String, T>> keyedMapToMapList(Map<String, Map<String, T>> keyedMap) {
+		return keyedMapToMapList(keyedMap, null, false);
+	}
+
+	/**
+	 * Trasforma una Mappa di Mappe in una Lista di Mappe ordinata.
+	 * @param keyedMap mappa da trasformare
+	 * @param orderKey chiave secondo la quale ordinare la lista
+	 * @return
+	 */
+	public static <T extends Object> List<Map<String, T>> keyedMapToMapList(Map<String, Map<String, T>> keyedMap, final String orderKey) {
+		return keyedMapToMapList(keyedMap, orderKey, false);
+	}
+	
+	/**
+	 * Trasforma una Mappa di Mappe in una Lista di Mappe ordinata.
+	 * @param keyedMap mappa da trasformare
+	 * @param orderKey chiave secondo la quale ordinare la lista
+	 * @param reverse flag per invertire l'ordine da crescente a decrescente
+	 * @return
+	 */
+	public static <T extends Object> List<Map<String, T>> keyedMapToMapList(Map<String, Map<String, T>> keyedMap, final String orderKey, final boolean reverse) {
+		List<Map<String, T>> retVal = new ArrayList<Map<String,T>>();
+		
+		for( Entry<String, Map<String, T>> aziEntry : keyedMap.entrySet() )
+			retVal.add(aziEntry.getValue());
+		
+		if (orderKey != null && !orderKey.isEmpty())
+			sortMapList(retVal, orderKey, reverse);
+		
+		return retVal;
+	}
+
+	/**
+	 * Ordina una lista di mappe.
+	 * @param mapList mappa da riordinare
+	 * @param orderKey chiave secondo la quale ordinare la lista
+	 * @return
+	 */
+	public static <T extends Object> void sortMapList(List<Map<String, T>> mapList, final String orderKey) {
+		sortMapList(mapList, orderKey, false);
+	}
+	
+	/**
+	 * Ordina una lista di mappe.
+	 * @param mapList mappa da riordinare
+	 * @param orderKey chiave secondo la quale ordinare la lista
+	 * @param reverse flag per invertire l'ordine da crescente a decrescente
+	 * @return
+	 */
+	public static <T extends Object> void sortMapList(List<Map<String, T>> mapList, final String orderKey, final Boolean reverse) {
+			Collections.sort(mapList, new Comparator<Map<String, T>>() {
+				@Override public int compare( Map<String, T> o1, Map<String, T> o2 ) {
+					String s1 = StrUtils.safeToStr(o1);
+					String s2 = StrUtils.safeToStr(o2);
+					if (reverse != null && reverse)
+						return s2.compareTo(s1);
+					else 
+						return s1.compareTo(s2);				
+				}
+			});
+	}
+	
+	/**
+	 * Cerca all'interno di una lista di mappe una mappa che contenga la coppia chiave/valore passati.
+	 * @param mapList lista di mappe in cui effettuare la ricerca
+	 * @param searchKey chiave del valore da cercare
+	 * @param searchValue valore cercato
+	 * @return
+	 */
+	public static <T extends Object> Map<String, T> findMap(List<Map<String, T>> mapList, String searchKey, String searchValue) {
+		for(Map<String, T> map : mapList) {
+			if (map.get(searchKey).equals(searchValue))
+				return map;
+		}
+		return null;
+	}
+	
+	/**
+	 * Cerca all'interno di una lista di mappe una mappa che contenga il valore passato, indipendentemente da quale chiave lo contenga.
+	 * @param mapList lista di mappe in cui effettuare la ricerca
+	 * @param searchValue valore cercato
+	 * @return
+	 */
+	public static <T extends Object> Map<String, T> findMap(List<Map<String, T>> mapList, String searchValue) {
+		for(Map<String, T> map : mapList) {
+			for( Entry<String, T> mapEntrySet : map.entrySet()) {
+				if (mapEntrySet.getValue().equals(searchValue))
+					return map;
+			}
+		}
+		return null;
 	}
 }
