b6d994273f378adb1bcd343b9bd8d2acf6ad4c6f
tosdan <stdanielets@gmail.com>
2015-10-13 23:20:30 +0200

    removed buildBeanFromRequest

--- WEB-INF/src/com/github/tosdan/utils/varie/HttpReuqestUtils.java
+++ WEB-INF/src/com/github/tosdan/utils/varie/HttpReuqestUtils.java
@@ -2,20 +2,8 @@ package com.github.tosdan.utils.varie;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
 
 /**
  * 
@@ -24,14 +12,9 @@ import com.google.gson.stream.JsonWriter;
  */
 public class HttpReuqestUtils {
 	
-	private final static Logger logger = LoggerFactory.getLogger(HttpReuqestUtils.class);
-	public final static String GSON_DATE_FORMAT = "HttpReuqestUtils.gsonDateFormat";
-	
 	public final static String[] sources = 
 		{"X-Forwarded-For", "X-FORWARDED-FOR", "x-forwarded-for", "Proxy-Client-IP" , "X-Forwarded-Server", "X-Forwarded-Host", "X-Pounded-For", "Proxy-Client-IP" , "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
 
-	private static final String APPLICATION_JSON = "application/json";
-	private static final String APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
 	
 	/**
 	 * 
@@ -96,130 +79,19 @@ public class HttpReuqestUtils {
 	 * @return
 	 */
 	public static String parseRequestBody(HttpServletRequest req) {
-		StringBuilder json = new StringBuilder();
+		StringBuilder body = new StringBuilder();
 		String line;
 		try {
 			
 			BufferedReader reqReader = req.getReader();
 			while ( (line = reqReader.readLine()) != null ) {
-				json.append(line);
+				body.append(line);
 			}
 			reqReader.close();
 			
 		} catch ( IOException e ) {
 			throw (RuntimeException) e.getCause();
 		}
-		return json.toString();
-	}
-
-	/**
-	 * Processa il contenuto di una {@link HttpServletRequest} e genera un oggetto con i parametri contenuti.
-	 * Supporta contentType <em>application/json</em> e <em>application/x-www-form-urlencoded</em>
-	 * @param req
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T buildBeanFromRequest(HttpServletRequest req, Class<T> clazz) {
-		
-		T retval = null;
-		
-		logger.debug("Classe oggetto Bean richiesto: [{}]", clazz.getName());
-		
-		String gsonDateFormat = getGsonDateFormat(req);
-		
-		Gson gson = new GsonBuilder()
-						.registerTypeAdapter(Double.class, new DoubleTypeAdapter())
-						.registerTypeAdapter(Float.class, new FloatTypeAdapter())
-						.setDateFormat(gsonDateFormat)
-						.create();
-		
-		String requestBody = HttpReuqestUtils.parseRequestBody(req);
-		logger.debug("Corpo della request: [{}]", requestBody);
-		
-		String contentType = req.getContentType();
-		logger.debug("ContentType: [{}]", contentType);
-		
-		String json = null;
-		
-		if ("GET".equalsIgnoreCase(req.getMethod())) {
-			logger.debug("Parsing QueryString parameters...");
-
-			Map<String, Object> requestParamsMap = QueryStringUtils.parse(req.getQueryString());
-//			logger.debug("Parametri della request: {}", requestParamsMap);
-			json = gson.toJson(requestParamsMap);
-			
-		} else if (StringUtils.containsIgnoreCase(contentType, APPLICATION_X_WWW_FORM_URLENCODED)) {
-			logger.debug("Parsing Form POST parameters...");
-			
-			Map<String, Object> requestParamsMap = QueryStringUtils.parse(requestBody);
-			logger.debug("Parametri della request: {}", requestParamsMap);
-			json = gson.toJson(requestParamsMap);
-			
-		} else if (StringUtils.containsIgnoreCase(contentType, APPLICATION_JSON)) {
-
-			json = requestBody;
-		}
-		
-		logger.debug("Json intermedio: {}", json);
-		
-		logger.debug("Creating instance...");
-		retval = gson.fromJson(json, clazz);
-		
-		return retval;
-	}
-
-	private static String getGsonDateFormat (HttpServletRequest req) {
-		String gsonDateFormat = req.getParameter(GSON_DATE_FORMAT);
-		if (gsonDateFormat == null) {
-			gsonDateFormat = (String) req.getAttribute(GSON_DATE_FORMAT);
-			if (gsonDateFormat == null) {
-				gsonDateFormat = "dd/MM/yyyy";
-			}
-		}
-		return gsonDateFormat;
-	}
-	
-	/**
-	 * Type adapter per consentire a {@link Gson} di effettuare il parse di cifre decimali con separatore virgola ","
-	 * @author Daniele
-	 *
-	 */
-	public static class DoubleTypeAdapter extends TypeAdapter<Double> {
-		@Override public Double read(JsonReader reader) throws IOException {
-			Double retval = null;
-            if (reader.peek() == JsonToken.NULL) { reader.nextNull(); }
-            String stringValue = reader.nextString();
-            try { 
-            	Double value = Double.valueOf(stringValue.replace(",", "."));
-                retval = value;
-            } catch (NumberFormatException e) { }
-            return retval;
-        }
-		@Override public void write(JsonWriter writer, Double value) throws IOException {
-            if (value == null) { writer.nullValue(); }
-            else { writer.value(value); }
-        }
-	}
-	
-	/**
-	 * Type adapter per consentire a {@link Gson} di effettuare il parse di cifre decimali con separatore virgola ","
-	 * @author Daniele
-	 *
-	 */
-	public static class FloatTypeAdapter extends TypeAdapter<Float> {
-		@Override public Float read(JsonReader reader) throws IOException {
-			Float retval = null;
-			if (reader.peek() == JsonToken.NULL) { reader.nextNull(); }
-			String stringValue = reader.nextString();
-			try { 
-				Float value = Float.valueOf(stringValue.replace(",", "."));
-				retval = value;
-			} catch (NumberFormatException e) { }
-			return retval;
-		}
-		@Override public void write(JsonWriter writer, Float value) throws IOException {
-			if (value == null) { writer.nullValue(); }
-			else { writer.value(value); }
-		}
+		return body.toString();
 	}
 }
